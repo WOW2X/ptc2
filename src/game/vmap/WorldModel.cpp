5855ea38a277e39f7c7b0f26c21b144617990631
xpearlis <xpearlis@localhost>
2015-12-31 01:28:22 +0100

    Models with flag MOD_M2 (mostly trees, among some other objects) should not be taken into account for LoS checks, this check does not apply to gameobjects (yet)
    Close Issue #21 ? LoS Issue for flag MOD_M2 (Mostly trees, among other objects)

--- src/game/vmap/WorldModel.cpp
+++ src/game/vmap/WorldModel.cpp
@@ -22,6 +22,8 @@
 #include "VMapDefinitions.h"
 #include "MapTree.h"
 
+#define MOD_M2 1
+
 using G3D::Vector3;
 using G3D::Ray;
 
@@ -421,6 +423,10 @@ namespace VMAP
 
     bool WorldModel::IntersectRay(const G3D::Ray &ray, float &distance, bool stopAtFirstHit) const
     {
+        // M2 models are not taken into account for LoS calculation
+        if (Flags & MOD_M2)
+            return false;
+
         // small M2 workaround, maybe better make separate class with virtual intersection funcs
         // in any case, there's no need to use a bound tree if we only have one submodel
         if (groupModels.size() == 1)
