a147dba8fbb956ef5d9955d5affb095c83efafa7
xpearlis <xpearlis@localhost>
2016-01-02 04:12:02 +0100

    Issue #22 ? Prince Malchezaar - Karazhan

--- src/game/SpellMgr.cpp
+++ src/game/SpellMgr.cpp
@@ -3453,6 +3453,10 @@ void SpellMgr::LoadSpellCustomAttr()
             case 30254: // Curator Evocation
                 spellInfo->Effect[1] = 0;   // remove self stun
                 break;
+            case 30843: // Enfeeble - Prince Malchezaar
+                spellInfo->EffectImplicitTargetA[0] = TARGET_UNIT_CASTER;
+                spellInfo->EffectImplicitTargetA[1] = TARGET_UNIT_CASTER;
+                break;
             case 29200: // Purify Helboar Meat
                 spellInfo->EffectImplicitTargetA[0] = TARGET_UNIT_CASTER;
                 break;
