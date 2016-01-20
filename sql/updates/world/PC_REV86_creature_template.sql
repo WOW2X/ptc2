7065eca53145251fe72ca265d1da4ed0779867a0
xpearlis <xpearlis@localhost>
2016-01-20 22:08:00 +0100

    Issue #44 ? CR: The Slave Pens

---
+++ sql/updates/world/PC_REV86_creature_template.sql
@@ -0,0 +1,3 @@
+UPDATE `creature_template` SET `maxlevel` = '63' WHERE `creature_template`.`entry` = 17963;
+UPDATE `creature_template` SET `maxlevel` = '63' WHERE `creature_template`.`entry` = 17964;
+UPDATE `creature_template` SET `maxlevel` = '64' WHERE `creature_template`.`entry` = 21127;
