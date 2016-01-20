03e454a8589575367b8b8ca3eddcb4dca6991628
xpearlis <xpearlis@localhost>
2016-01-20 22:11:44 +0100

    Issue #133 ? deeprun tram floating crates

---
+++ sql/updates/world/PC_REV88_gameobject.sql
@@ -0,0 +1,3 @@
+UPDATE `gameobject` SET `position_z` = '-4.297363' WHERE `gameobject`.`guid` = 20738;
+UPDATE `gameobject` SET `position_z` = '-4.297363' WHERE `gameobject`.`guid` = 20739;
+UPDATE `gameobject` SET `position_z` = '-4.297363' WHERE `gameobject`.`guid` = 20740;
