a7bf67b449c81c8d8f29939e79cfa35e4ee9ffe8
xpearlis <xpearlis@localhost>
2016-01-20 22:15:40 +0100

    Issue #137 ? Ironforge bailey stonemantle missing

---
+++ sql/updates/world/PC_REV89_creature.sql
@@ -0,0 +1,2 @@
+INSERT INTO `creature` (`guid`, `id`, `map`, `spawnMask`, `modelid`, `equipment_id`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, `spawndist`, `currentwaypoint`, `curhealth`, `curmana`, `DeathState`, `MovementType`) VALUES 
+('NULL', '2461', '0', '1', '0', '0', '-4886.636719', '-997.468384', '503.940369', '2.303830', '540', '5', '0', '7300', '0', '0', '0');
