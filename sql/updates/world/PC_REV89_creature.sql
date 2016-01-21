b214c6ac945a6ec26345a28a0ceea31a89b61f40
xpearlis <xpearlis@localhost>
2016-01-21 22:50:45 +0100

    Damianc0 Choose Your Path quests + very tiny cleanup of sql updates

--- sql/updates/world/PC_REV89_creature.sql
+++ sql/updates/world/PC_REV89_creature.sql
@@ -1,2 +1,3 @@
+DELETE FROM `creature` WHERE `creature`.`id` = 2461;
 INSERT INTO `creature` (`guid`, `id`, `map`, `spawnMask`, `modelid`, `equipment_id`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, `spawndist`, `currentwaypoint`, `curhealth`, `curmana`, `DeathState`, `MovementType`) VALUES 
 (NULL, 2461, 0, 1, 0, 0, -4886.636719, -997.468384, 503.940369, 2.303830, 540, 0, 0, 7300, 0, 0, 0);
--- sql/updates/world/PC_REV92_creature.sql
+++ sql/updates/world/PC_REV92_creature.sql
@@ -1,2 +1,3 @@
+DELETE FROM `creature` WHERE `creature`.`id` = 12977;
 INSERT INTO `creature` (`guid`, `id`, `map`, `spawnMask`, `modelid`, `equipment_id`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, `spawndist`, `currentwaypoint`, `curhealth`, `curmana`, `DeathState`, `MovementType`) VALUES 
 (NULL, 12977, 369, 1, 0, 0, -17.5448, 42.2011, -4.29736, 4.7447, 300, 0, 0, 2600, 0, 0, 0);
