ad56ca979e59da763976805cec916ab4bde10dc0
xpearlis <xpearlis@localhost>
2016-02-11 20:40:43 +0100

    Issue #17 ? Flamewaker Imp

---
+++ sql/updates/world/PC_REV145_creature_formations.sql
@@ -0,0 +1,70 @@
+DELETE FROM `creature_formations` WHERE `creature_formations`.`leaderGUID` = 68433;
+INSERT INTO `creature_formations` (`leaderGUID`, `memberGUID`, `dist`, `angle`, `groupAI`) VALUES
+(68433, 68433, 0, 0, 2),
+(68433, 68249, 3, 360, 2),
+(68433, 68250, 3, 360, 2);
+DELETE FROM `creature_formations` WHERE `creature_formations`.`leaderGUID` = 68434;
+INSERT INTO `creature_formations` (`leaderGUID`, `memberGUID`, `dist`, `angle`, `groupAI`) VALUES
+(68434, 68434, 0, 0, 2),
+(68434, 68251, 3, 360, 2),
+(68434, 68252, 3, 360, 2);
+DELETE FROM `creature_formations` WHERE `creature_formations`.`leaderGUID` = 68409;
+INSERT INTO `creature_formations` (`leaderGUID`, `memberGUID`, `dist`, `angle`, `groupAI`) VALUES
+(68409, 68409, 0, 0, 2),
+(68409, 68201, 3, 360, 2),
+(68409, 68202, 3, 360, 2);
+DELETE FROM `creature_formations` WHERE `creature_formations`.`leaderGUID` = 68408;
+INSERT INTO `creature_formations` (`leaderGUID`, `memberGUID`, `dist`, `angle`, `groupAI`) VALUES
+(68408, 68408, 0, 0, 2),
+(68408, 68199, 3, 360, 2),
+(68408, 68200, 3, 360, 2);
+DELETE FROM `creature_formations` WHERE `creature_formations`.`leaderGUID` = 68406;
+INSERT INTO `creature_formations` (`leaderGUID`, `memberGUID`, `dist`, `angle`, `groupAI`) VALUES
+(68406, 68406, 0, 0, 2),
+(68406, 68195, 3, 360, 2),
+(68406, 68196, 3, 360, 2);
+DELETE FROM `creature_formations` WHERE `creature_formations`.`leaderGUID` = 68407;
+INSERT INTO `creature_formations` (`leaderGUID`, `memberGUID`, `dist`, `angle`, `groupAI`) VALUES
+(68407, 68407, 0, 0, 2),
+(68406, 68197, 3, 360, 2),
+(68406, 68198, 3, 360, 2);
+DELETE FROM `creature_formations` WHERE `creature_formations`.`leaderGUID` = 68429;
+INSERT INTO `creature_formations` (`leaderGUID`, `memberGUID`, `dist`, `angle`, `groupAI`) VALUES
+(68429, 68429, 0, 0, 2),
+(68429, 68242, 3, 360, 2),
+(68429, 68243, 3, 360, 2);
+DELETE FROM `creature_formations` WHERE `creature_formations`.`leaderGUID` = 68428;
+INSERT INTO `creature_formations` (`leaderGUID`, `memberGUID`, `dist`, `angle`, `groupAI`) VALUES
+(68428, 68428, 0, 0, 2),
+(68428, 68240, 3, 360, 2),
+(68428, 68241, 3, 360, 2);
+DELETE FROM `creature_formations` WHERE `creature_formations`.`leaderGUID` = 68430;
+INSERT INTO `creature_formations` (`leaderGUID`, `memberGUID`, `dist`, `angle`, `groupAI`) VALUES
+(68430, 68430, 0, 0, 2),
+(68430, 68244, 3, 360, 2),
+(68430, 68245, 3, 360, 2);
+DELETE FROM `creature_formations` WHERE `creature_formations`.`leaderGUID` = 68431;
+INSERT INTO `creature_formations` (`leaderGUID`, `memberGUID`, `dist`, `angle`, `groupAI`) VALUES
+(68431, 68431, 0, 0, 2),
+(68431, 68239, 3, 360, 2),
+(68431, 68246, 3, 360, 2);
+DELETE FROM `creature_formations` WHERE `creature_formations`.`leaderGUID` = 68435;
+INSERT INTO `creature_formations` (`leaderGUID`, `memberGUID`, `dist`, `angle`, `groupAI`) VALUES
+(68435, 68435, 0, 0, 2),
+(68435, 68253, 3, 360, 2),
+(68435, 68254, 3, 360, 2);
+DELETE FROM `creature_formations` WHERE `creature_formations`.`leaderGUID` = 68432;
+INSERT INTO `creature_formations` (`leaderGUID`, `memberGUID`, `dist`, `angle`, `groupAI`) VALUES
+(68432, 68432, 0, 0, 2),
+(68432, 68247, 3, 360, 2),
+(68432, 68248, 3, 360, 2);
+DELETE FROM `creature_formations` WHERE `creature_formations`.`leaderGUID` = 68427;
+INSERT INTO `creature_formations` (`leaderGUID`, `memberGUID`, `dist`, `angle`, `groupAI`) VALUES
+(68427, 68427, 0, 0, 2),
+(68427, 68237, 3, 360, 2),
+(68427, 68238, 3, 360, 2);
+DELETE FROM `creature_formations` WHERE `creature_formations`.`leaderGUID` = 68426;
+INSERT INTO `creature_formations` (`leaderGUID`, `memberGUID`, `dist`, `angle`, `groupAI`) VALUES
+(68426, 68426, 0, 0, 2),
+(68426, 68235, 3, 360, 2),
+(68426, 68236, 3, 360, 2);
