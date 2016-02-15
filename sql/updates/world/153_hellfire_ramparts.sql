677f4bf5e37d26f4f9f62458ec502c9028f80a5d
xpearlis <xpearlis@localhost>
2016-02-15 02:14:59 +0100

    Hellfire Ramparts another corrections

---
+++ sql/updates/world/153_hellfire_ramparts.sql
@@ -0,0 +1,11 @@
+-- Corrected language of aggro trash texts
+UPDATE `creature_ai_texts` SET `language` = '0' WHERE `creature_ai_texts`.`entry` = -156; -- language correction
+UPDATE `creature_ai_texts` SET `language` = '0' WHERE `creature_ai_texts`.`entry` = -181; -- language correction 
+UPDATE `creature_ai_texts` SET `language` = '0' WHERE `creature_ai_texts`.`entry` = -672; -- language correction
+
+UPDATE `creature_template` SET `speed` = '1.1' WHERE `creature_template`.`entry` = 17264; -- speed correction
+UPDATE `creature_template` SET `speed` = '1.1' WHERE `creature_template`.`entry` = 17280; -- speed correction
+ 
+UPDATE `waypoint_data` SET `delay` = '1000' WHERE `waypoint_data`.`id` = 1411 AND `waypoint_data`.`point` = 6; -- 1 sec stop at 6 point
+
+UPDATE `creature` SET `spawndist`=2,`MovementType`=1 WHERE `guid`= 200941; -- random movement
