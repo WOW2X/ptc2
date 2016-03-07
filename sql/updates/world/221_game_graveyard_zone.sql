51c96bf80601994887cee330e059e981223f1ce0
unspecifiedx <unspecified@localhost>
2016-03-07 01:14:31 +0100

    Issue #469 ? [WORLD] [P1] Wrong ressurection location in HFP

---
+++ sql/updates/world/221_game_graveyard_zone.sql
@@ -0,0 +1,2 @@
+UPDATE `game_graveyard_zone` SET `faction` = '67' WHERE `game_graveyard_zone`.`id` = 919 AND `game_graveyard_zone`.`ghost_zone` = 3713;
+UPDATE `game_graveyard_zone` SET `faction` = '469' WHERE `game_graveyard_zone`.`id` = 920 AND `game_graveyard_zone`.`ghost_zone` = 3713; 
