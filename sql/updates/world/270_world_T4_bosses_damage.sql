e3fd5a5dba54900f368229ddb6711a1243a97f0a
xpearlis <xpearlis@localhost>
2016-04-10 00:14:14 +0200

    T4 bosses damage corrections

---
+++ sql/updates/world/270_world_T4_bosses_damage.sql
@@ -0,0 +1,83 @@
+-- Gruul's Lair
+UPDATE `creature_template` SET `mindmg` = '1200' WHERE `creature_template`.`entry` = 18832;  -- Krosh Firehand min
+UPDATE `creature_template` SET `maxdmg` = '1635' WHERE `creature_template`.`entry` = 18832;  -- Krosh Firehand max
+UPDATE `creature_template` SET `mindmg` = '3742' WHERE `creature_template`.`entry` = 18834;  -- Olm the Summoner min
+UPDATE `creature_template` SET `maxdmg` = '5099' WHERE `creature_template`.`entry` = 18834;  -- Olm the Summoner max
+UPDATE `creature_template` SET `mindmg` = '6707' WHERE `creature_template`.`entry` = 18835;  -- Kiggler the Crazed min
+UPDATE `creature_template` SET `maxdmg` = '9139' WHERE `creature_template`.`entry` = 18835;  -- Kiggler the Crazed max
+UPDATE `creature_template` SET `mindmg` = '1871' WHERE `creature_template`.`entry` = 18836;  -- Blindeye the Seer min
+UPDATE `creature_template` SET `maxdmg` = '2549' WHERE `creature_template`.`entry` = 18836;  -- Blindeye the Seer max
+UPDATE `creature_template` SET `mindmg` = '12065' WHERE `creature_template`.`entry` = 18831; -- High King Maulgar min
+UPDATE `creature_template` SET `maxdmg` = '16454' WHERE `creature_template`.`entry` = 18831; -- High King Maulgar max
+UPDATE `creature_template` SET `mindmg` = '10875' WHERE `creature_template`.`entry` = 19044; -- Gruul the Dragonkiller min
+UPDATE `creature_template` SET `maxdmg` = '12875' WHERE `creature_template`.`entry` = 19044; -- Gruul the Dragonkiller max
+
+-- Magtheridon
+UPDATE `creature_template` SET `mindmg` = '23003' WHERE `creature_template`.`entry` = 17257; -- Magtheridon min
+UPDATE `creature_template` SET `maxdmg` = '27234' WHERE `creature_template`.`entry` = 17257; -- Magtheridon max
+
+-- Karazhan
+UPDATE `creature_template` SET `mindmg` = '7731' WHERE `creature_template`.`entry` = 15550;  -- Attumen The Huntsman Ground min
+UPDATE `creature_template` SET `maxdmg` = '9686' WHERE `creature_template`.`entry` = 15550;  -- Attumen The Huntsman Ground max
+UPDATE `creature_template` SET `mindmg` = '5444' WHERE `creature_template`.`entry` = 16152;  -- Attumen The Huntsman Mounted min
+UPDATE `creature_template` SET `maxdmg` = '6229' WHERE `creature_template`.`entry` = 16152;  -- Attumen The Huntsman Mounted max
+UPDATE `creature_template` SET `mindmg` = '6690' WHERE `creature_template`.`entry` = 16151;  -- Midnight min
+UPDATE `creature_template` SET `maxdmg` = '7922' WHERE `creature_template`.`entry` = 16151;  -- Midnight max
+UPDATE `creature_template` SET `mindmg` = '7529' WHERE `creature_template`.`entry` = 15687;  -- Moroes min
+UPDATE `creature_template` SET `maxdmg` = '8913' WHERE `creature_template`.`entry` = 15687;  -- Moroes max
+UPDATE `creature_template` SET `mindmg` = '3321' WHERE `creature_template`.`entry` = 19875;  -- Baroness Dorothea Millstipe min
+UPDATE `creature_template` SET `maxdmg` = '3931' WHERE `creature_template`.`entry` = 19875;  -- Baroness Dorothea Millstipe max
+UPDATE `creature_template` SET `mindmg` = '3321' WHERE `creature_template`.`entry` = 19872;  -- Lady Catriona Von'Indi min
+UPDATE `creature_template` SET `maxdmg` = '3931' WHERE `creature_template`.`entry` = 19872;  -- Lady Catriona Von'Indi max
+UPDATE `creature_template` SET `mindmg` = '3321' WHERE `creature_template`.`entry` = 17007;  -- Lady Keira Berrybuck min
+UPDATE `creature_template` SET `maxdmg` = '3931' WHERE `creature_template`.`entry` = 17007;  -- Lady Keira Berrybuck max
+UPDATE `creature_template` SET `mindmg` = '411' WHERE `creature_template`.`entry` = 19874;   -- Baron Rafe Dreuger min
+UPDATE `creature_template` SET `maxdmg` = '488' WHERE `creature_template`.`entry` = 19874;   -- Baron Rafe Dreuger max
+UPDATE `creature_template` SET `mindmg` = '3579' WHERE `creature_template`.`entry` = 19876;  -- Lord Robin Daris min
+UPDATE `creature_template` SET `maxdmg` = '4238' WHERE `creature_template`.`entry` = 19876;  -- Lord Robin Daris max
+UPDATE `creature_template` SET `mindmg` = '3579' WHERE `creature_template`.`entry` = 19873;  -- Lord Crispin Ference min
+UPDATE `creature_template` SET `maxdmg` = '4238' WHERE `creature_template`.`entry` = 19873;  -- Lord Crispin Ference max
+UPDATE `creature_template` SET `mindmg` = '9695' WHERE `creature_template`.`entry` = 16457;  -- Maiden of Virtue min
+UPDATE `creature_template` SET `maxdmg` = '11472' WHERE `creature_template`.`entry` = 16457; -- Maiden of Virtue max
+UPDATE `creature_template` SET `mindmg` = '7529' WHERE `creature_template`.`entry` = 16179;  -- Hyakiss the Lurker min
+UPDATE `creature_template` SET `maxdmg` = '8913' WHERE `creature_template`.`entry` = 16179;  -- Hyakiss the Lurker max
+UPDATE `creature_template` SET `mindmg` = '5647' WHERE `creature_template`.`entry` = 16181;  -- Rokad the Ravager min
+UPDATE `creature_template` SET `maxdmg` = '6685' WHERE `creature_template`.`entry` = 16181;  -- Rokad the Ravager max
+UPDATE `creature_template` SET `mindmg` = '7529' WHERE `creature_template`.`entry` = 16180;  -- Shadikith the Glider min
+UPDATE `creature_template` SET `maxdmg` = '8913' WHERE `creature_template`.`entry` = 16180;  -- Shadikith the Glider max
+UPDATE `creature_template` SET `mindmg` = '3879' WHERE `creature_template`.`entry` = 17535;  -- Dorothee min
+UPDATE `creature_template` SET `maxdmg` = '4589' WHERE `creature_template`.`entry` = 17535;  -- Dorothee max
+UPDATE `creature_template` SET `mindmg` = '1326' WHERE `creature_template`.`entry` = 17548;  -- Tito min
+UPDATE `creature_template` SET `maxdmg` = '1569' WHERE `creature_template`.`entry` = 17548;  -- Tito max 
+UPDATE `creature_template` SET `mindmg` = '3952' WHERE `creature_template`.`entry` = 17546;  -- Roar min
+UPDATE `creature_template` SET `maxdmg` = '4680' WHERE `creature_template`.`entry` = 17546;  -- Roar max
+UPDATE `creature_template` SET `mindmg` = '6588' WHERE `creature_template`.`entry` = 17543;  -- Strawman min
+UPDATE `creature_template` SET `maxdmg` = '7801' WHERE `creature_template`.`entry` = 17543;  -- Strawman max
+UPDATE `creature_template` SET `mindmg` = '6588' WHERE `creature_template`.`entry` = 17547;  -- Tinhead min
+UPDATE `creature_template` SET `maxdmg` = '7801' WHERE `creature_template`.`entry` = 17547;  -- Tinhead max
+UPDATE `creature_template` SET `mindmg` = '7756' WHERE `creature_template`.`entry` = 18168;  -- The Crone min
+UPDATE `creature_template` SET `maxdmg` = '9178' WHERE `creature_template`.`entry` = 18168;  -- The Crone max
+UPDATE `creature_template` SET `mindmg` = '6273' WHERE `creature_template`.`entry` = 17533;  -- Romulo min
+UPDATE `creature_template` SET `maxdmg` = '7427' WHERE `creature_template`.`entry` = 17533;  -- Romulo max
+UPDATE `creature_template` SET `mindmg` = '4653' WHERE `creature_template`.`entry` = 17534;  -- Julianne min
+UPDATE `creature_template` SET `maxdmg` = '5506' WHERE `creature_template`.`entry` = 17534;  -- Julianne max
+UPDATE `creature_template` SET `mindmg` = '6524' WHERE `creature_template`.`entry` = 17521;  -- Big Bad Wolf min
+UPDATE `creature_template` SET `maxdmg` = '7725' WHERE `creature_template`.`entry` = 17521;  -- Big Bad Wolf max
+UPDATE `creature_template` SET `mindmg` = '8609' WHERE `creature_template`.`entry` = 15691;  -- The Curator min
+UPDATE `creature_template` SET `maxdmg` = '10188' WHERE `creature_template`.`entry` = 15691; -- The Curator max
+UPDATE `creature_template` SET `mindmg` = '3276' WHERE `creature_template`.`entry` = 16524;  -- Shade of Aran min
+UPDATE `creature_template` SET `maxdmg` = '3926' WHERE `creature_template`.`entry` = 16524;  -- Shade of Aran max
+UPDATE `creature_template` SET `mindmg` = '7955' WHERE `creature_template`.`entry` = 15688;  -- Terestian Illhoof min
+UPDATE `creature_template` SET `maxdmg` = '9418' WHERE `creature_template`.`entry` = 15688;  -- Terestian Illhoof max
+UPDATE `creature_template` SET `mindmg` = '4344' WHERE `creature_template`.`entry` = 17229;  -- Kil'rek min
+UPDATE `creature_template` SET `maxdmg` = '5028' WHERE `creature_template`.`entry` = 17229;  -- Kil'rek max
+UPDATE `creature_template` SET `mindmg` = '8567' WHERE `creature_template`.`entry` = 15689;  -- Netherspite min
+UPDATE `creature_template` SET `maxdmg` = '11707' WHERE `creature_template`.`entry` = 15689; -- Netherspite max 
+UPDATE `creature_template` SET `mindmg` = '10454' WHERE `creature_template`.`entry` = 15690; -- Prince Malchezaar min
+UPDATE `creature_template` SET `maxdmg` = '12378' WHERE `creature_template`.`entry` = 15690; -- Prince Malchezaar max
+UPDATE `creature_template` SET `mindmg` = '461' WHERE `creature_template`.`entry` = 17650;   -- Prince Malchezaar Axes min
+UPDATE `creature_template` SET `maxdmg` = '643' WHERE `creature_template`.`entry` = 17650;   -- Prince Malchezaar Axes max
+UPDATE `creature_template` SET `mindmg` = '16216' WHERE `creature_template`.`entry` = 17225; -- Nightbane min
+UPDATE `creature_template` SET `maxdmg` = '18825' WHERE `creature_template`.`entry` = 17725; -- Nightbane max
+UPDATE `creature_template` SET `mindmg` = '2623' WHERE `creature_template`.`entry` = 17261;  -- Restless Skeleton min
+UPDATE `creature_template` SET `maxdmg` = '3242' WHERE `creature_template`.`entry` = 17261;  -- Restless Skeleton max
