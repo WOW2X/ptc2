b888fc559ad5e7c3e3e99c24a049bc754143c439
xpearlis <xpearlis@localhost>
2016-04-11 23:54:30 +0200

    Add missing guid to Isle of Quel Danas event.

---
+++ sql/updates/world/285_isle_quel_danas.sql
@@ -0,0 +1,4 @@
+DELETE FROM `game_event_creature` WHERE `game_event_creature`.`guid` = 96600;
+ 
+INSERT INTO `game_event_creature` (`guid`, `event`) VALUES ('96600', '35'); 
+INSERT INTO `game_event_creature` (`guid`, `event`) VALUES ('96600', '36'); 
